70a5d5a74224e413bbe0d1975ef7e9ce0b23f7a3
Priyam Sanodiya <119919234+priyam930@users.noreply.github.com>
2025-02-05 22:06:36 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 document.addEventListener('visibilitychange',
     function() {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Bhavishya Sharma";
+            document.title = "Portfolio | Priyam Sanodiya";
             $("#favicon").attr("href", "assets/images/favicon.png");
         } else {
             document.title = "Come Back To Portfolio";

6edd795306f168d3bf47276fe8e5abc184b60259
Priyam Sanodiya <119919234+priyam930@users.noreply.github.com>
2025-02-09 16:11:03 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["MlOps Enthusiast", "DevOps Enthusiast"],
+    strings: ["MlOps Engineer", "DevOps Engineer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
